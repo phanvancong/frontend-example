296c7672964fe22291500e5bb9b48a17282e2077
pvcong <pvcong@gmail.com>
2019-11-23 23:45:58 +0700

    Subscribe without switchMap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import style from './main.css';
+import { Observable, fromEvent, interval } from 'rxjs';
 
-const myArrowFunc = (a, b) => {
-  if (a === void 0) return b;
-  return a;
-};
+//example of switchMap from https://www.youtube.com/watch?v=6lKoLwGlglE
+let button = document.querySelector("#smButton");
+let obs1 = fromEvent(button, 'click');
+let obs2 = interval(1000);
+let counter = 0;
+obs1.subscribe(
+  event => {
+    counter++;
+    obs2.subscribe(i => {
+      console.log('i ', counter, ' ', i)
+    })
+  }
+);
