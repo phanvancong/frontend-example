27d589945685d161056d0c52274a6c1284260b26
pvcong <pvcong@gmail.com>
2019-11-23 23:56:52 +0700

    Subscribe with switchMap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import style from './main.css';
 import { Observable, fromEvent, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 //example of switchMap from https://www.youtube.com/watch?v=6lKoLwGlglE
 let button = document.querySelector("#smButton");
 let obs1 = fromEvent(button, 'click');
 let obs2 = interval(1000);
 let counter = 0;
-obs1.subscribe(
-  event => {
-    counter++;
-    obs2.subscribe(i => {
-      console.log('i ', counter, ' ', i)
-    })
-  }
+// obs1.subscribe(
+//   event => {
+//     counter++;
+//     obs2.subscribe(i => {
+//       console.log('i ', counter, ' ', i)
+//     })
+//   }
+// );
+obs1.pipe(
+  switchMap(
+    e => obs2
+  )
+).subscribe(
+  i => console.log(i),
+  err => console.log(err),
+  () => console.log('complete')
 );
